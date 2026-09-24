476fbf1c362de2267dcad37edf0373bb755ac5cc
texxid <gracelopez918@icloud.com>
2026-09-24 16:50:14 +0000

    ⭐new

--- s02/s01T0401_for.py
+++ s02/s01T0401_for.py
@@ -9,29 +9,38 @@ calculara la suma del 1 al 100
 # Importamos biblioteca time
 import time
 
-#Tomando el tiempo inicial 
-timestamp_01 = time.time()
+#Funcion que suma los primeros "n" numeros naturales
+def sum_of_n(n):
+    total_sum = 0
+    #Sumando los "n" números
+    #Ciclo for
+    for number in range(1,n+1):
+        total_sum = total_sum + number
+    #Retornando el total de la suma
+    return total_sum
 
-#Progrma que calcula la suma
-# de los "n" numeros naturales
-n = 100
-total_sum = 0
+#Variable para guardar el data set
+dataset = [] #((n, time))
 
-# Ciclo for para la suma
-for number in range(1,n+1):
-    total_sum = total_sum + number
-    #1: sum <- 0 + 1
-    #sum = 1
-    #2: suma <- 1 + 2
-    #sum = 3
-    #3: suma <- 1 + 3
-    #sum = 4
-    #...
-    #100: sum <- antSum + 100
-print(f"la suma de 1 hasta {n} es: {total_sum}")
+#Como guardar el contenido del dataset
+for repetition in range(1,11):
+    #⏱️Tomando el tiempo inicial 
+    timestamp_01 = time.time()
 
-#Tomando el timepo final 
-timestamp_02 = time.time()
+    #Sumo los "n" numeros
+    n = repetition*500
+    #Guardo mi resultado en result
+    result = sum_of_n(n)
 
-#Impresión del tiempo de ejecución 
-print(f"Tiempo de ejecución: {(timestamp_02-timestamp_01) * 1e6:.2f} μs")
+    #⏱️Tomando el timepo final 
+    timestamp_02 = time.time()
+
+    #Impresión del tiempo de ejecución
+    elapsed_time = round((timestamp_02-timestamp_01) * 1e6,2)
+
+    #Agregar la tripreta de los datos a dataset
+    dataset.append((n,elapsed_time,result))
+
+#
+for tup in dataset:
+    print(tup)
